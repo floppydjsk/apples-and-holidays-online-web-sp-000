a0f1ae18793e4eba1f02b903f2a92b2400cd4fd3
Daniel Kim <daniel.kim52@gmail.com>
2020-03-10 17:05:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |day, supplies|
+      puts "  #{holiday.to_s.split('_').map {|letter| letter.capitalize}.join(' ')}: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
